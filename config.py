2bbdde15c3c704515b0e902448e1cc55002b2581
Mikhail <47355726+Holdfast12@users.noreply.github.com>
2021-12-22 07:43:22 +0300

    123

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 TOKEN = 'faef561afaef561afaef561ad9fa95d8a8ffaeffaef561a9b2ffa6df093c6f298cf65a8'
 PROTTOKEN = 'V2DpB331lMAvTNFqmI7I'
+
+#https://oauth.vk.com/authorize?client_id=XXX&response_type=token
 TEMPTOKEN = '3a41f8b9907aa76969cbe5fca208f4e695f09f7ff1fd749b61c08bd217ce61ad72840b93911ef197ab135'
