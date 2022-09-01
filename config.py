e62b6b5aaab9df1b1177be158d28d575eb56f1f0
Mikhail <47355726+Holdfast12@users.noreply.github.com>
2022-09-01 11:52:06 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = 'faef561afaef561afaef561ad9fa95d8a8ffaeffaef561a9b2ffa6df093c6f298cf65a8'
-PROTTOKEN = 'V2DpB331lMAvTNFqmI7I'
-
-#https://oauth.vk.com/authorize?client_id=XXX&response_type=token
-TEMPTOKEN = '2b5ae896bb81009a73a5eeaeb059c133398ea1f7a931a3e0c02543c6a9fb1d826e4126ba89d874e11359f'
